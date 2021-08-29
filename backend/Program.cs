e63f5f6a7c9a6cc3df967ee1a2ac3c33b44f407e
Elliott Record <elliottrecord@gmail.com>
2021-08-29 12:21:43 +0100

    Added aspmvc project

--- backend/Program.cs
+++ backend/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace aspnetweb
+namespace aspmvc
 {
     public class Program
     {
