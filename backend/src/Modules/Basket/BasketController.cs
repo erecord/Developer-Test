dad8519724a98cd1c14851447bf1c3fe992417b1
Elliott Record <elliottrecord@gmail.com>
2021-09-03 18:02:18 +0100

    Refactored controllers to use the repositories

---
+++ backend/src/Modules/Basket/BasketController.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreBackend.Facade;
+using StoreBackend.Interfaces;
+using StoreBackend.Models;
+
+namespace StoreBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+        private readonly IBasketProductRepository _basketProductRepository;
+        private readonly BasketControllerFacade _controllerFacade;
+
+        public BasketController(
+            IBasketRepository repository, IBasketProductRepository basketProductRepository,
+            BasketControllerFacade basketControllerFacade
+        )
+        {
+            _basketRepository = repository;
+            _basketProductRepository = basketProductRepository;
+            _controllerFacade = basketControllerFacade;
+        }
+
+        // GET: api/Basket
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Basket>>> GetBasket() => Ok(await _basketRepository.GetAllAsync());
+
+        // GET: api/Basket/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Basket>> GetBasket(int id)
+        {
+            var basket = await (await _basketRepository.IncludeAsync(b => b.Discount)).FirstAsync(p => p.Id.Equals(id));
+
+            if (basket == null)
+            {
+                return NotFound();
+            }
+
+            basket.Products = (await _controllerFacade.QueryProductsInBasketCommand.Execute(id)).ToList();
+
+            return basket;
+        }
+
+        // PUT: api/Basket/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBasket(int basketId, UpdateBasketDTO updateBasketDTO)
+        {
+            if (basketId != updateBasketDTO.Basket.Id)
+            {
+                return BadRequest();
+            }
+
+            var productIdsInBasket = (await _controllerFacade.QueryProductIdsInBasketCommand.Execute(basketId)).ToList();
+
+            var removedProductIds = productIdsInBasket.Except(updateBasketDTO.ProductIds);
+            var newProductIds = updateBasketDTO.ProductIds.Except(productIdsInBasket);
+
+            await _controllerFacade.AddProductsToBasketCommand.Execute((basketId, newProductIds));
+            await _controllerFacade.RemoveProductsFromBasketCommand.Execute(removedProductIds);
+
+            var entityFound = await _basketRepository.UpdateAsync(updateBasketDTO.Basket);
+
+            if (!entityFound)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Basket
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Basket>> PostBasket(Basket basket)
+        {
+            await _basketRepository.CreateAsync(basket);
+
+            return CreatedAtAction("GetBasket", new { id = basket.Id }, basket);
+        }
+
+        // DELETE: api/Basket/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBasket(int id)
+        {
+            var basket = await _basketRepository.GetOneAsync(id);
+            if (basket == null)
+            {
+                return NotFound();
+            }
+
+            await _basketRepository.DeleteAsync(basket);
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ backend/src/Modules/Product/ProductController.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoreBackend.Interfaces;
+using StoreBackend.Models;
+
+namespace StoreBackend
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _productRepository = repository;
+        }
+
+        // GET: api/Product
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProduct() => Ok(await _productRepository.GetAllAsync());
+
+        // GET: api/Product/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _productRepository.GetOneAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // PUT: api/Product/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            var entityFound = await _productRepository.UpdateAsync(product);
+
+            if (!entityFound)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Product
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            await _productRepository.CreateAsync(product);
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+        }
+
+        // DELETE: api/Product/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _productRepository.GetOneAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            await _productRepository.DeleteAsync(product);
+
+            return NoContent();
+        }
+
+    }
+}
