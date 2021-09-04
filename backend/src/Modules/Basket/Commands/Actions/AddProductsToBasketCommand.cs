53d8f1242252b3e2eb4b3f960e3ff88e62076dfc
Elliott Record <elliottrecord@gmail.com>
2021-09-04 12:18:47 +0100

    Updated AddProductsToBasketCommand

--- backend/src/Modules/Basket/Commands/Actions/AddProductsToBasketCommand.cs
+++ backend/src/Modules/Basket/Commands/Actions/AddProductsToBasketCommand.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,22 +9,39 @@ namespace StoreBackend.Commands
     public class AddProductsToBasketCommand : IAddProductsToBasketCommand
     {
         private readonly IBasketProductRepository _basketProductRepository;
+        private readonly IBasketRepository _basketRepository;
+        private readonly IProductRepository _productRepository;
         private readonly IBasketProductFactory _basketProductFactory;
 
-        public AddProductsToBasketCommand(IBasketProductRepository basketProductRepository, IBasketProductFactory basketProductFactory)
+        public AddProductsToBasketCommand(
+            IBasketProductRepository basketProductRepository,
+            IBasketRepository basketRepository,
+            IProductRepository productRepository,
+            IBasketProductFactory basketProductFactory)
         {
             _basketProductRepository = basketProductRepository;
+            _basketRepository = basketRepository;
+            _productRepository = productRepository;
             _basketProductFactory = basketProductFactory;
         }
 
         public async Task Execute((int basketId, IEnumerable<int> newProductIds) parameters)
         {
-            await Task.Run(() =>
-                parameters.newProductIds.ToList().ForEach(async productId =>
+            await Task.Run(async () =>
+            {
+                var basket = await _basketRepository.GetOneAsync(parameters.basketId);
+                if (basket == null)
+                    throw new InvalidOperationException($"The basket with id '{parameters.basketId}' does not exist.");
+
+                foreach (var productId in parameters.newProductIds.ToList())
                 {
+                    var product = await _productRepository.GetOneAsync(productId);
+                    if (product == null)
+                        throw new InvalidOperationException($"The product with id '{productId}' does not exist.");
+
                     await _basketProductRepository.CreateAsync(_basketProductFactory.CreateBasketProduct(parameters.basketId, productId));
-                })
-            );
+                }
+            });
         }
     }
 }
