5d2423db5a0f83ccf7b4dae9a48786f82e0bea3e
Elliott Record <elliottrecord@gmail.com>
2021-09-04 23:46:31 +0100

    Added NotFound Exceptions for basket and discount

---
+++ backend/src/Modules/Basket/Exceptions/BasketNotFoundException.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace StoreBackend.Exceptions
+{
+    public class BasketNotFoundException : Exception
+    {
+    }
+}
---
+++ backend/src/Modules/Discount/Exceptions/DiscountCodeNotFoundException.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace StoreBackend.Exceptions
+{
+    public class DiscountCodeNotFoundException : Exception
+    {
+    }
+}
