f0280edd0372118b33d0221cbb4db70ada62da9a
Elliott Record <elliottrecord@gmail.com>
2021-09-04 14:27:36 +0100

    Updated command to check for basket existence explicity

--- backend/src/Modules/Basket/Commands/Queries/QueryProductIdsInBasketCommand.cs
+++ backend/src/Modules/Basket/Commands/Queries/QueryProductIdsInBasketCommand.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using StoreBackend.Interfaces;
@@ -7,15 +8,25 @@ namespace StoreBackend.Commands
     public class QueryProductIdsInBasketCommand : IQueryProductIdsInBasketCommand
     {
         private readonly IBasketProductRepository _basketProductRepository;
+        private readonly IBasketRepository _basketRepository;
 
-        public QueryProductIdsInBasketCommand(IBasketProductRepository basketProductRepository)
+        public QueryProductIdsInBasketCommand(
+            IBasketProductRepository basketProductRepository,
+            IBasketRepository basketRepository
+            )
         {
             _basketProductRepository = basketProductRepository;
+            _basketRepository = basketRepository;
         }
 
         public async Task<IQueryable<int>> Execute(int basketId)
         {
-            var productIdsInBasket = (await _basketProductRepository.WhereAsync(bp => bp.BasketId == basketId)).Select(bp => bp.ProductId);
+            var basket = await _basketRepository.GetOneAsync(basketId);
+            if (basket == null)
+                throw new InvalidOperationException($"The basket with id {basketId} does not exist");
+
+            var basketProducts = await _basketProductRepository.WhereAsync(bp => bp.BasketId.Equals(basketId));
+            var productIdsInBasket = basketProducts.Select(bp => bp.ProductId);
             return productIdsInBasket;
         }
     }
