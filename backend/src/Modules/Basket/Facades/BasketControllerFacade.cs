29877154d179a16fce973060207e58194e6ec382
Elliott Record <elliottrecord@gmail.com>
2021-09-05 12:21:54 +0100

    Added BasketDiscountService and QueryTotalCostOfBasketCommand to basketControllerFacade

--- backend/src/Modules/Basket/Facades/BasketControllerFacade.cs
+++ backend/src/Modules/Basket/Facades/BasketControllerFacade.cs
@@ -10,17 +10,23 @@ namespace StoreBackend.Facade
             IQueryProductsInBasketCommand queryProductsInBasketCommand,
             IQueryProductIdsInBasketCommand queryProductIdsInBasketCommand,
             IRemoveProductsFromBasketCommand removeProductsFromBasketCommand,
-            IAddProductsToBasketCommand addProductIdsToBasketCommand
+            IAddProductsToBasketCommand addProductIdsToBasketCommand,
+            IBasketDiscountService basketDiscountService,
+            IQueryTotalCostOfBasketCommand queryTotalCostOfBasketCommand
         )
         {
             QueryProductsInBasketCommand = queryProductsInBasketCommand;
             QueryProductIdsInBasketCommand = queryProductIdsInBasketCommand;
             RemoveProductsFromBasketCommand = removeProductsFromBasketCommand;
             AddProductsToBasketCommand = addProductIdsToBasketCommand;
+            BasketDiscountService = basketDiscountService;
+            QueryTotalCostOfBasketCommand = queryTotalCostOfBasketCommand;
         }
         public IQueryProductsInBasketCommand QueryProductsInBasketCommand { get; }
         public IQueryProductIdsInBasketCommand QueryProductIdsInBasketCommand { get; }
         public IRemoveProductsFromBasketCommand RemoveProductsFromBasketCommand { get; }
         public IAddProductsToBasketCommand AddProductsToBasketCommand { get; }
+        public IBasketDiscountService BasketDiscountService { get; }
+        public IQueryTotalCostOfBasketCommand QueryTotalCostOfBasketCommand { get; }
     }
 }
